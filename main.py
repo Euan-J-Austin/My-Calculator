eecd10144cc00d6b3be4913412148c633e2c30b3
Euan Austin <elvismonarchy@proton.me>
2023-03-07 07:14:32 +0000

    Added current output

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 #Input - active input (values you're inputting right now , stored in input (current computation)
 
-#Modify input -- CE (clear most recenty entry), C (clear all input)
-
 #Memory -- MS (memory store), MR (recall stored numbers), M+ means add presently displayed value to value in memory changing the value in memory, M- substract present value from stored value changing the value in memory, MC (memory clear)
+
+#Modify input -- CE (clear most recenty entry), C (clear all input)
 
 #Calculations class - addition, subtraction, multiplication, division , BIDMAS 
 
@@ -12,15 +12,27 @@
 
 # INPUT active input - input a value and arithmetical operator, this is sent to stored 
 
-active_input = input("Input sinlge operand or operator: ")
-#this allows for order of operations?
+#can just enter al input at once and have user confirm it is correct? does python account for order of ops? 
+
+active_input = input("Input all operands and operators: ")
 stored_input = []
+
+def current_output(active_input):
+  try:
+    if active_input[1] == '+':
+      return int(active_input[0])+int(active_input[2])
+  except TypeError:
+    pass
 
 while active_input != 'Calculate':
   stored_input.append(active_input)
   print(f'Stored input is {stored_input}')
-  active_input = input("Input single operand or operator: ")
+  print(f'Current output is {current_output(active_input)}')
+  active_input = input("Input all operands and operators: ")
 else:
   pass
 
 print(stored_input)
+
+#can you convert a string to binary and sum?
+
