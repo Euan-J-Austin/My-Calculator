92daab3d47a56d80b47f5ac3a4b4237862d84e39
Euan Austin <elvismonarchy@proton.me>
2023-03-08 22:26:14 +0000

    Classful, order of operations, pruned entry class, tail recursion

--- main.py
+++ main.py
@@ -10,43 +10,71 @@
 
 #Output -- print values
 
-# INPUT active input - input a value and arithmetical operator, this is sent to stored 
-
-#can just enter al input at once and have user confirm it is correct? does python account for order of ops? 
+#aesthetics --- clearing screen with every active_input entry? 
 
 
 stored_input = []
+memory = []
 
-def current_output(active_input):
-  try:
-    if active_input[1] == '+':
-      return int(active_input[0])+int(active_input[2])
-  except TypeError:
+class calculateOutput:
+  def __init__(self):
     pass
 
-def clear_entry(stored_input):
-  del stored_input[-1]
-  return print(stored_input[0:])
-  
+  def current_output(si):
+    #need try test here 
+    return eval(''.join(si))
 
-def clear_all(stored_input):
-  stored_input.clear()
-  return print(stored_input)
+class modifyStoredInput:
+  def __init__(self):
+    pass
+  def modify(actinp):
+    if actinp == 'CE':
+      del stored_input[-1]
+      print(stored_input[0:])
+      active.entry(input("Enter all operands and operators: "))
+    else:
+      stored_input.clear()
+      print(stored_input)
+      active.entry(input("Enter all operands and operators: "))
 
-def entry(active_input):
-    if active_input == 'CE':
-      clear_entry(stored_input)
-    elif active_input == 'C':
-      clear_all(stored_input)
-    elif active_input == 'Execute':
+class memoryUtility:
+  def __init(self):
+    pass
+  def modify(actinp):
+    if actinp == 'MC':
+      memory.clear()
+    elif actinp == 'MR':
       pass
-    elif active_input != 'CE' and 'C' and 'Execute':
-      for o in active_input:
-        stored_input.append(o)
-      print(f'Stored input is {stored_input}')
-      print(f'Current output is {current_output(active_input)}')
-      entry(active_input = input("Enter all operands and operators: "))
+    elif actinp == 'M+':
+      sum(actinp)
+    elif actinp == 'M-':
+      pass
+    elif actinp == 'MS':
+      pass
 
-entry(active_input = input("Enter all operands and operators: "))
+class activeEntry(modifyStoredInput, calculateOutput):
+  def __init__(self):
+    pass
+  def entry(self, actinp):
+      if actinp[0] == 'C':
+        modifyStoredInput.modify(actinp)
+      elif actinp[0] == 'M':
+        memoryUtility.modify(actinp)
+        pass
+      elif actinp == 'Execute':
+        pass
+        #use map function? 
+      elif actinp != 'CE' and 'C' and 'Execute':
+        for o in actinp:
+          if o != ' ':
+            stored_input.append(o)
+        print(f'Stored input is {stored_input}')
+        print(f'Current output is {calculateOutput.current_output(stored_input)}')
+        self.entry(input("Enter all operands and operators: "))
 
+active = activeEntry()
+modify = modifyStoredInput()
+calculate = calculateOutput()
+
+active.entry(input("Enter all operands and operators: "))
 
