f8cb7970b71219c4de267fa3a854a5da7ba4672a
Euan Austin <elvismonarchy@proton.me>
2023-03-13 22:08:04 +0000

    Simplifying classes, data types, location of evaluation

--- main.py
+++ main.py
@@ -1,108 +1,42 @@
-stored_input = []
-memory = ['0']
+MR = ['0']
+stored = []
 
-class calculateOutput:
-  pass
-  # def __init__(self, si):
-  #   self.x = eval(''.join(si))
-
-  # def current_output(self, si):
-  #   #need try test here 
-  #   return self.si
+class activeInput:
+  def __init__(self):
+    pass
+  def enter(i):
+    #evaluates to numeric is true?
+    if i[0] == 'C':
+      storedInput.modify_stored(i)
+    if i[-2:] == 'M':
+      activeMemory.modify_memory(i)
+    if i[0] != 'C' or i[-2:] != 'M':
+      for x in i:
+        if x != ' ':
+          stored.append(x)
+      print(stored)
+      print(eval(''.join(stored)))
+      activeInput.enter(input('Enter: '))
   
-  # def execute(self, si):
-  #   return print(f'The final value is: {self.x}.')
-
-class modifyStoredInput:
+class activeMemory:
   def __init__(self):
     pass
-  def modify(actinp):
-    if actinp == 'CE':
-      del stored_input[-1]
-      print(stored_input[0:])
-      active.entry(input("Enter all operands and operators: "))
-    else: #if actinp is equal to C
-      stored_input.clear()
-      print(stored_input)
-      active.entry(input("Enter all operands and operators: "))
+  def modify_memory(self, x):
+    if x == 'MS':
+      MR.clear()
+      for n in stored:
+        MR.append(stored)
+      print(MR)
+    if x == 'MC':
+      MR.clear()
 
-class memoryUtility:
-  def __init(self):
-    pass
-  def modify(actinp):
-    if actinp == 'MC':
-      memory.clear()
-    if actinp == '+MR' or '-MR' or '*MR' or '/MR' or '**MR':
-      #either operating on stored input e.g. actinp = '+MR' or operation producing value
-      #for stored_input e.g. '+7*MR'
-      if actinp == '+MR':
-        stored_input.append('+')
-        stored_input.append(eval(''.join(actinp[0:-3])) + eval(''.join(memory)))
-      if actinp == '-MR':
-        stored_input.append(eval(''.join(actinp[0:-3])) - eval(''.join(memory)))
-      if actinp == '*MR':
-        stored_input.append(eval(''.join(actinp[0:-3])) * eval(''.join(memory)))
-      if actinp == '/MR':
-        stored_input.append(eval(''.join(actinp[0:-3])) / eval(''.join(memory)))
-      if actinp == '**MR':
-        stored_input.append(eval(''.join(actinp[0:-4])) ** eval(''.join(memory)))
-    if actinp == 'M+':
-      new_mem= eval(''.join(memory)) + eval(''.join(stored_input))
-      memory.clear()
-      memory.append(new_mem)
-      print(memory)
-      active.entry(input("Enter all operands and operators: "))
-    if actinp == 'M-':
-      new_mem = eval(''.join(memory)) - eval(''.join(stored_input))
-      memory.clear()
-      memory.append(new_mem)
-      print(memory)
-      active.entry(input("Enter all operands and operators: "))
-    if actinp == 'MS':
-      memory.clear()
-      memory.append(eval(''.join(stored_input)))
-      print(memory)
-      active.entry(input("Enter all operands and operators: "))
-
-class activeEntry(modifyStoredInput): # calculateOutput
+class storedInput:
   def __init__(self):
     pass
-  def entry(self, actinp):
-      if actinp[0] == 'C':
-        modifyStoredInput.modify(actinp)
-      if actinp[-1:] or actinp[-2:] == 'M':
-        memoryUtility.modify(actinp)
-      # elif actinp == 'Execute':
-      #   calculateOutput.current_output(stored_input)
-      if actinp[0].isnumeric() or actinp[1].isnumeric() or actinp[2].isnumeric() is True:
-        for o in actinp:
-          if o != ' ':
-            stored_input.append(o)
-        print(f'Stored input is {stored_input}')
-        print(f"Current output is {eval(''.join(stored_input))}")
-        self.entry(input("Enter all operands and operators: "))
-
-active = activeEntry()
-modify = modifyStoredInput()
-# calculate = calculateOutput()
-
-active.entry(input("Enter all operands and operators: "))
-
-      #this solves the memory recall operations issues 
-
-# si = ['1']
-
-# memory = ['1']
-
-# actinp = '+1+MR'
-
-# if actinp[0] == '+' or '-' or '*' or '/' or '**':
-#   si.append(actinp[0])
-# if actinp[-3:] == '+MR':
-#   try:
-#     si.append(str(eval(''.join(actinp[0:-3])) + eval(''.join(memory))))
-#   except SyntaxError:
-#     si.append(str(eval(''.join(memory))))
-#   print(si)
-#   print(type(si))
-#   print(eval(''.join(si)))
+  def modify_stored(self, x):
+    if x == 'C':
+      stored.clear()
+    if x == 'CE':
+      del stored[-1]
+                 
+activeInput.enter(input('Enter: '))
