062b31b0aee01aa1305b5d89fa37b6597af4e709
Euan Austin <elvismonarchy@proton.me>
2023-03-14 06:15:27 +0000

    Error handling, scope issues

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-MR = ['0']
-stored = []
-
 class activeInput:
   def __init__(self):
     pass
   def enter(i):
-    #evaluates to numeric is true?
-    if i[0] == 'C':
-      storedInput.modify_stored(i)
-    if i[-2:] == 'M':
-      activeMemory.modify_memory(i)
-    if i[0] != 'C' or i[-2:] != 'M':
-      for x in i:
-        if x != ' ':
-          stored.append(x)
+    try: 
+      if i[0] == 'C':
+        storedInput.modify_stored(i)
+      elif i[-2] == 'M':
+          activeMemory.modify_memory(i)
+      else:
+        for x in i:
+          if x != ' ':
+            stored.append(x)
+            print(stored)
+            activeInput.enter(input('Enter: '))
+    except IndexError: #deals with case of single operand or operator
+      stored.append(i)
       print(stored)
-      print(eval(''.join(stored)))
       activeInput.enter(input('Enter: '))
-  
+
 class activeMemory:
   def __init__(self):
     pass
-  def modify_memory(self, x):
+  def modify_memory(x):
     if x == 'MS':
-      MR.clear()
-      for n in stored:
-        MR.append(stored)
-      print(MR)
+      MR = stored
+      print(f'test {MR}')
+      activeInput.enter(input('Enter: '))
     if x == 'MC':
-      MR.clear()
+      MR = []
+      print(f'test1 {MR}')
+      activeInput.enter(input('Enter: '))
 
 class storedInput:
   def __init__(self):
     pass
-  def modify_stored(self, x):
+  def modify_stored(x):
     if x == 'C':
       stored.clear()
+      print(f'test stored C {stored}')
+      activeInput.enter(input('Enter: '))
     if x == 'CE':
       del stored[-1]
-                 
+      print(f'test stored CE {stored}')
+      activeInput.enter(input('Enter: '))
+
+MR = []
+stored = []
+
 activeInput.enter(input('Enter: '))
